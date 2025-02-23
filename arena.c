f460532524409d772f6b56fd32d222bc88f3abcf
Connor Walstrom <walst110@umn.edu>
2025-02-22 22:30:08 -0600

    Add license and readme

--- arena.c
+++ arena.c
@@ -1,29 +1,4 @@
-/* The concept of an arena/linear allocator is pretty basic, in that all you do is pre-allocate
-// some amount of memory, then instead of calling _alloc() functions, you use arena_alloc
-// functions to simply grab some amount of memory from the arena. Doing this offers 2 primary
-// benefits: a significantly easier to understand memory lifetime, and an ease of deallocation
-
-// Memory lifetime is how long memory is supposed to be allocated. The lifetime of a variable local
-// to a function call is until the function returns. Once it does, the memory is marked free and
-// can be reused. The _alloc functions put lifetime management in your hands, which can be quite
-// nice because you can have ANY lifetime you want, but it can also be a handful to keep track of
-// each and every single thing you have allocated. Using an arena to do your allocation makes it
-// easier to keep track because you know it's just based on the arena's lifetime
-
-// As for the deallocation, arenas are actually slightly weird in how they work. You're not allowed
-// to / can't deallocate individual bits of arena-allocated memory. You can only deallocate the
-// entire arena at once. However, again, not only is this generally nice, but it plays into that
-// concept of a defined lifetime, and makes it easier to keep track of everything
-
-// Not everything needs to be allocated in an arena, but it's a tool that most find useful because
-// it simplifies a lot about memory management (if implemented well)
-
-// Implementation wise, there are may ways to skin this particular cat, however the most common ones
-// use some form of linked list (usually singly-linked) and an arena object. The arena object tends
-// to be an opaque structure which manages the linked list of arena nodes, and the nodes deal with
-// the allocated memory
-
-*/
+// Consult the readme on what an arena allocator is (from my limited knowledge)
 
 #include "arena.h"
 
--- arena.h
+++ arena.h
@@ -1,6 +1,8 @@
 #ifndef __ARENA_ALLOCATOR___ARENA_H___17052168762686___
 #define __ARENA_ALLOCATOR___ARENA_H___17052168762686___
 
+typedef struct arena arena;
 
+int arena_init(arena **a, size_t bytes);
 
 #endif

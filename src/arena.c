c801404eed365c6eb61b5a1d8fe464665b1858c9
Connor Walstrom <walst110@umn.edu>
2025-02-23 01:42:17 -0600

    Little bit of touch up

--- src/arena.c
+++ src/arena.c
@@ -25,24 +25,21 @@ typedef struct arena {
 } arena;
 
 int arenanode_init(arenanode **an, size_t bytes) {
-    // Create a base arenanode
     (*an) = malloc(1 * sizeof(**an));
     if(!(*an))
         return -1;
 
-    // Set some values
     (*an)->allocated = bytes;
     (*an)->used = 0;
     (*an)->next = NULL;
     
-    // Allocate memory for the node to dish out
     void *mem = malloc(bytes);
     if(!mem) {
         free((*an));
+        (*an) = NULL;
         return -1;
     }
 
-    // Put the memory into the node
     (*an)->membase = mem;
     (*an)->memcur = mem;
 
@@ -50,7 +47,6 @@ int arenanode_init(arenanode **an, size_t bytes) {
 }
 
 int arena_init(arena **a, size_t bytes) {
-    // Create an arena object
     (*a) = malloc(1 * sizeof(**a));
     if(!(*a))
         return -1;
@@ -58,14 +54,13 @@ int arena_init(arena **a, size_t bytes) {
     (*a)->current = NULL;
     (*a)->node_memspace = bytes;
 
-    // Create the base arena node & preallocate some memory
     arenanode *base;
     if(arenanode_init(&base, bytes) < 0) {
         free(*a);
+        (*a) = NULL;
         return -1;
     }
 
-    // Put the base arenanode into the arena
     (*a)->start = base;
     (*a)->current = base;
 
@@ -80,10 +75,6 @@ void* arena_alloc(arena * const arena, size_t bytes) {
         return NULL;
     }
 
-    // Check if there's enough room in the current node:
-        // Enough       - Increment the necessary values and return the desired memory
-        // Not enough   - Make a new arenanode, append it to the list, and increment everything
-
     if(bytes > ((arena->current)->allocated - (arena->current)->used)) {
         arenanode *new;
         if(arenanode_init(&new, arena->node_memspace) < 0)
@@ -95,11 +86,6 @@ void* arena_alloc(arena * const arena, size_t bytes) {
 
     void *mem = arena->current->memcur;
     arena->current->memcur = (void*)(((uint8_t*)arena->current->memcur) + bytes); 
-        // I believe this is the "correct" way to do void pointer arithmetic. Technically, there is no correct way, but if I'm
-        // remembering how array pointer arithmetic works, all that's happening when dereferencing an array via x[y] notation is
-        // *(x + y), which if x is the size of a byte and y is the number of bytes, the correct way to do it for void*'s is
-        // *((uint8_t*)x + (i * sizeof(element))). If you don't care about some specific element size and just want raw bytes,
-        // it's simplified a little into *((uint8_t*)x + i)
 
     arena->current->used += bytes;
 
@@ -107,6 +93,8 @@ void* arena_alloc(arena * const arena, size_t bytes) {
 }
 
 int arena_free(arena **arena) {
+    if(!arena)
+        return -1;
     if(!(*arena))
         return -1;
 
